5778376a3a474f351b00481b6002cc6ef46868a9
Kamil <72623198+Skriptoo@users.noreply.github.com>
2023-11-23 20:31:00 +0100

    project init

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { ChakraBaseProvider } from '@chakra-ui/react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <></>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraBaseProvider>
+      <RouterProvider router={router}/>
+    </ChakraBaseProvider>
   </React.StrictMode>,
 )
